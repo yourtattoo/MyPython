0ca8392057eadce3f9ffcf3aeb376674b9580685
yourtattoo <513447539@qq.com>
2017-03-13 20:02:14 +0800

    add a dict

--- 1.py
+++ 1.py
@@ -4,3 +4,6 @@ def add(x,y):
 x = float(raw_input("pls enter the 1st number:\n"))
 y = float(raw_input("pls enter the 2ed number:\n"))
 print add(x,y)
+d = dict(name = "george",age = "24")
+d.setdefault('address',[]).append("No.50")
+print d
